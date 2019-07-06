237d41a6015dd8869829de535d0e92ada542c4e0
pradhapakilan <52598242+pradhapakilan@users.noreply.github.com>
2019-07-06 16:30:17 +0530

    Add files via upload

---
+++ sorting(descending).c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+
+#include<conio.h>
+
+void main()
+
+{
+   
+ int i,j,size,temp;
+  
+  int a[10];
+    
+printf("enter the number size\n");
+
+    scanf("%d",&size);
+    
+printf("enter the elments\n");
+    
+for(i=0;i<size;i++)
+
+    {
+       
+ scanf("%d",&a[i]);
+  
+  }
+    
+for(i=0;i<size;i++)
+
+    {
+       
+ for(j=i+1;j<size;j++)
+ 
+       {
+          
+         
+ if(a[i]<a[j])
+   
+     {
+          
+  temp=a[i];
+ 
+   a[i]=a[j];
+           
+ a[j]=temp;
+       
+ }
+        }
+    }
+    
+        
+printf("the sorted number is:\n");
+       
+ for(i=0;i<size;i++)
+       
+ {
+           
+ printf("%d\n",a[i]);
+      
+  }
+        
+     
+   
+ getch();
+
+}
