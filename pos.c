4bf984135ac41ae39d02ef9f9579057b14f1650f
pradhapakilan <52598242+pradhapakilan@users.noreply.github.com>
2019-07-22 15:52:08 +0530

    Add files via upload

---
+++ pos.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+#include<conio.h>
+
+int main()
+
+{
+    
+int n;
+   
+ scanf("%d",&n);
+   
+ if(n>0)
+   
+ {
+        
+printf("%d number is positive",n);
+    
+}
+    
+else if(n<0)
+   
+ {
+       
+ printf("%d number is negative",n);
+
+ }
+else
+   
+ {
+        
+printf("the number is zero");
+   
+ }
+    
+return 0;
+
+}
