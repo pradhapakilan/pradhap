5a2c1ccc59b811739835133bd34b68732f6079e5
pradhapakilan <52598242+pradhapakilan@users.noreply.github.com>
2019-07-22 17:19:35 +0530

    Add files via upload

---
+++ oddeven.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+#include<conio.h>
+
+int main()
+
+{
+    
+int n;
+   
+scanf("%d",&n);
+if(n<0)
+{
+    printf("invalid");
+}
+  
+ else if(n%2==0)
+    
+{
+        
+printf("even");
+    
+}
+    
+else
+    
+{
+        
+printf("odd");
+    
+}
+    
+   
+ return 0;
+
+}
