45801e16a725538ba93f2e090555113435e1e571
pradhapakilan <52598242+pradhapakilan@users.noreply.github.com>
2019-07-22 16:49:37 +0530

    Add files via upload

---
+++ ala.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char n;
+    int lowercase,uppercase;
+    scanf("%c",&n);
+    lowercase=(n=='a'||n=='e'||n=='i'||n=='o'||n=='u');
+    uppercase=(n=='A'||n=='E'||n=='I'||n=='O'||n=='U');
+    if(lowercase || uppercase)
+    {
+        printf("alaphabet");
+        else
+        printf("constant")
+    }
+    return 0;
+}
+    
