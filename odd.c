9d94e4ba87640caf40ca62cab1fb16b57771330d
pradhapakilan <52598242+pradhapakilan@users.noreply.github.com>
2019-07-22 16:09:34 +0530

    Add files via upload

---
+++ odd.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+#include<conio.h>
+
+int main()
+
+{
+    
+int n;
+   
+scanf("%d",&n);
+  
+ if(n%2==0)
+    
+{
+        
+printf("even");
+    
+}
+    
+else
+    
+{
+        
+printf("odd");
+    
+}
+    
+   
+ return 0;
+
+}
