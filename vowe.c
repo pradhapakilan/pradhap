a81edc2ac9fac489e9c16337681233a802a76b74
pradhapakilan <52598242+pradhapakilan@users.noreply.github.com>
2019-07-22 17:14:08 +0530

    Add files via upload

---
+++ vowe.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char n;
+    scanf("%c",&n);
+    if(n=='$')
+    {
+        printf("Invalid");
+        
+    }
+    else if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u')
+    {
+        printf("vowel");
+    }
+    else 
+    {
+        printf("consonant");
+    }
+    
+    return 0;
+}
+    
